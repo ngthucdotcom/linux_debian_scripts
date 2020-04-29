89ca6368bec5f6bfb7abe7a6e0501d9caf4b298d
Nguyen Thuc <lenguyenthuc.edu@gmail.com>
2020-04-29 11:20:52 +0700

    Update linux_install_scripts.sh

--- linux_install_scripts.sh
+++ linux_install_scripts.sh
@@ -4,62 +4,97 @@ sudo apt-get update
 sudo sleep 1
 
 # install packages
+echo ''
 echo '***************************'
 echo 'Install packages processing'
 echo '***************************'
-sudo apt-get install -y git curl docker.io docker-compose openvpn snap
+echo ''
+sudo apt-get install -y git curl docker.io docker-compose openvpn snap wget
 
 sudo sleep 1
 
 # Re-update
+echo ''
 echo '***************************'
 echo 'Update processing'
 echo '***************************'
+echo ''
 sudo apt autoremove
 sudo apt-get update
 
 sudo sleep 1
 
 # install nodejs & npm
+echo ''
 echo '***************************'
 echo 'Install node.js'
 echo '***************************'
+echo ''
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
+echo ''
 echo '***************************'
 echo 'Use npm without sudo'
 echo '***************************'
+echo ''
 mkdir ~/.npm-global
 npm config set prefix '~/.npm-global'
 export PATH=~/.npm-global/bin:$PATH
 source ~/.profile
+sudo chown -R $USER:$(id -gn $USER) /home/$USER/.config
 
 sudo sleep 1
 
 # install snapshot
+echo ''
 echo '***************************'
 echo 'Install snapshots'
 echo '***************************'
+echo ''
 
+echo ''
 echo '***************************'
-echo '1. figma'
+echo '1. visual studio code'
 echo '***************************'
+echo ''
+sudo snap install code --classic
+
+sudo sleep 1
+
+echo ''
+echo '***************************'
+echo '2. gitkraken'
+echo '***************************'
+echo ''
+sudo snap install gitkraken
+
+sudo sleep 1
+
+echo ''
+echo '***************************'
+echo '3. figma'
+echo '***************************'
+echo ''
 sudo snap install figma-linux
 
 sudo sleep 1
 
+echo ''
 echo '***************************'
-echo '2. postman'
+echo '4. postman'
 echo '***************************'
+echo ''
 sudo snap install postman
 
 sudo sleep 1
 
 # install unikey
+echo ''
 echo '***************************'
 echo 'Install unikey'
 echo '***************************'
+echo ''
 sudo add-apt-repository ppa:teni-ime/ibus-teni
 sudo apt-get update
 sudo apt-get install -y ibus-teni
@@ -67,13 +102,17 @@ ibus restart
 
 sudo sleep 1
 
+echo ''
 echo '***************************'
 echo 'Install successfully!'
 echo '***************************'
+echo ''
 
 # Restart
+echo ''
 echo '***************************'
-echo 'Auto restart after 3s'
+echo 'Auto restart after 5s'
 echo '***************************'
-sudo sleep 3
+echo ''
+sudo sleep 5
 sudo init 6
